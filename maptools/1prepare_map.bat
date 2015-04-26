74efbf31770c7dc27a5ba14c97c97a865e5c3547
PJB3005 <pieterjan.briers@gmail.com>
2015-04-26 12:44:54 +0200

    Adds dorf.dmm to the mapmerger batch files

--- maptools/1prepare_map.bat
+++ maptools/1prepare_map.bat
@@ -3,6 +3,7 @@ set MAPFILE_VG=vgstation.dmm
 set MAPFILE_EFF=defficiency.dmm
 set MAPFILE_TAX=taxistation.dmm
 set MAPFILE_MS=metaclub.dmm
+set MAPFILE_DF=dorf.dmm
 
 cd ../maps
 copy %MAPFILE_TG% %MAPFILE_TG%.backup
@@ -10,5 +11,6 @@ copy %MAPFILE_VG% %MAPFILE_VG%.backup
 copy %MAPFILE_EFF% %MAPFILE_EFF%.backup
 copy %MAPFILE_TAX% %MAPFILE_TAX%.backup
 copy %MAPFILE_MS% %MAPFILE_MS%.backup
+copy %MAPFILE_DF% %MAPFILE_DF%.backup
 
 pause
