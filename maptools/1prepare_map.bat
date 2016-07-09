5b513098cfba6cfd1c8c5ee6546c45ea1e9cfdf0
PJB3005 <pieterjan.briers@gmail.com>
2016-07-09 19:26:08 +0200

    maptools

--- maptools/1prepare_map.bat
+++ maptools/1prepare_map.bat
@@ -3,7 +3,6 @@ set MAPFILE_EFF=defficiency.dmm
 set MAPFILE_TAX=taxistation.dmm
 set MAPFILE_MS=metaclub.dmm
 set MAPFILE_MIN=ministation.dmm
-set MAPFILE_BUS=busstation.dmm
 
 cd ../maps
 copy %MAPFILE_TG% %MAPFILE_TG%.backup
@@ -11,6 +10,5 @@ copy %MAPFILE_EFF% %MAPFILE_EFF%.backup
 copy %MAPFILE_TAX% %MAPFILE_TAX%.backup
 copy %MAPFILE_MS% %MAPFILE_MS%.backup
 copy %MAPFILE_MIN% %MAPFILE_MIN%.backup
-copy %MAPFILE_BUS% %MAPFILE_BUS%.backup
 
 pause
