a5cbfdf2c09968f085fac1436481225adfa8aedc
PJB3005 <pieterjan.briers@gmail.com>
2016-07-18 18:58:39 +0200

    Removes Box

--- maptools/1prepare_map.bat
+++ maptools/1prepare_map.bat
@@ -1,4 +1,3 @@
-set MAPFILE_TG=tgstation.dmm
 set MAPFILE_EFF=defficiency.dmm
 set MAPFILE_TAX=taxistation.dmm
 set MAPFILE_MS=metaclub.dmm
@@ -6,7 +5,6 @@ set MAPFILE_MIN=ministation.dmm
 set MAPFILE_BUS=busstation.dmm
 
 cd ../maps
-copy %MAPFILE_TG% %MAPFILE_TG%.backup
 copy %MAPFILE_EFF% %MAPFILE_EFF%.backup
 copy %MAPFILE_TAX% %MAPFILE_TAX%.backup
 copy %MAPFILE_MS% %MAPFILE_MS%.backup
--- maptools/1prepare_map.sh
+++ maptools/1prepare_map.sh
@@ -1,4 +1,3 @@
-export MAPFILE_TG=tgstation.dmm
 export MAPFILE_EFF=defficiency.dmm
 export MAPFILE_TAX=taxistation.dmm
 export MAPFILE_MS=metaclub.dmm
@@ -6,9 +5,7 @@ export MAPFILE_MIN=ministation.dmm
 
 cd ../maps
 
-cp $MAPFILE_TG $MAPFILE_TG.backup
 cp $MAPFILE_EFF $MAPFILE_EFF.backup
 cp $MAPFILE_TAX $MAPFILE_TAX.backup
 cp $MAPFILE_MS $MAPFILE_MS.backup
 cp $MAPFILE_MIN $MAPFILE_MIN.backup
- 
