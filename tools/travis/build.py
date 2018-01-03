8bb0214ed0aa4a74b59d160164227c79a5a4ce69
PJB3005 <pieterjan.briers@gmail.com>
2018-01-03 18:19:06 +0100

    Attempt 2: asyncio galore.

--- tools/travis/build.py
+++ tools/travis/build.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python
-from __future__ import print_function, unicode_literals
+#!/usr/bin/env python3
 import argparse
-import re
+import asyncio
 import distutils.spawn
-import os
+import re
+import subprocess
 import sys
+import typing
 
-try:    
-    import subprocess32 as subprocess
-except ImportError:
-    import subprocess
+MAP_INCLUDE_RE = re.compile(r"#include \"maps\\[a-zA-Z0-9][a-zA-Z0-9_]*\.dm\"")
 
 
 def main():
@@ -23,18 +21,17 @@ def main():
         # Handle map file replacement.
         with open(dme, "r") as f:
             content = f.read()
-        
+
         # Make string to replace the map include with.
         includes = ""
         for arg in args.mapfile:
             includes += "#include \"maps\\\\{}.dm\"\n".format(arg)
-        
-        MAP_INCLUDE_RE = re.compile(r"#include \"maps\\[a-zA-Z0-9][a-zA-Z0-9_]*\.dm\"")
+
         content = MAP_INCLUDE_RE.sub(includes, content, count=1)
         dme = "{}.mdme".format(dme)
         with open(dme, "w") as f:
             f.write(content)
-    
+
     compiler = "DreamMaker"
     if sys.platform == "win32" or sys.platform == "cygwin":
         compiler = "dm"
@@ -44,8 +41,26 @@ def main():
         print("Unable to find DM compiler.")
         exit(1)
 
-    code = subprocess.call([compiler, dme])
+    loop = asyncio.get_event_loop()
+    code = loop.run_until_complete([compiler, *args])
     exit(code)
+
+# DM SOMEHOW manages to go 10 minutes without logging anything nowadays.
+# So... Travis kills it.
+# Thanks DM.
+# This repeats messages like travis_wait (which I couldn't get working) does to prevent that.
+async def run_compiler(args: typing.List[str]) -> int:
+    compiler_process = await asyncio.create_subprocess_exec(*args)
+    task = asyncio.ensure_future(print_timeout_guards())
+
+    ret = await compiler_process.wait()
+    task.cancel()
+    return ret
+
+async def print_timeout_guards():
+    while True:
+        print("Keeping Travis alive. Ignore this!")
+        await asyncio.sleep(120)
 
 if __name__ == "__main__":
     main()
