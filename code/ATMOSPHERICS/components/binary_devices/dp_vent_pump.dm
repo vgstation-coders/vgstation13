vgstation-coders/vgstation13

commit 79ec1d0fe01b3b98386532d6745e3d594db94548

Stops testing() abuse ACT2 (#18549)
* Stops testing() abuse ACT2

* Easy weird trick.

code/ATMOSPHERICS/components/binary_devices/dp_vent_pump.dm
@@ -200,8 +200,8 @@
 		spawn(2)
 			broadcast_status()
 		return //do not update_icon
-	if(!handled)
-		testing("\[[world.timeofday]\]: dp_vent_pump/receive_signal: unknown command \n[signal.debug_print()]")
+//	if(!handled)
+		//testing("\[[world.timeofday]\]: dp_vent_pump/receive_signal: unknown command \n[signal.debug_print()]")
 	spawn(2)
 		broadcast_status()
 	update_icon()

code/ATMOSPHERICS/components/unary/vent_pump.dm
@@ -279,8 +279,8 @@
 			broadcast_status()
 		return //do not update_icon
 
-	if(!handled)
-		testing("\[[world.timeofday]\]: vent_pump/receive_signal: unknown command \n[signal.debug_print()]")
+//	if(!handled)
+		//testing("\[[world.timeofday]\]: vent_pump/receive_signal: unknown command \n[signal.debug_print()]")
 	spawn(2)
 		broadcast_status()
 	update_icon()

code/ATMOSPHERICS/atmospherics.dm
@@ -239,7 +239,7 @@ Pipelines + Other Objects -> Pipe network
 				continue
 			var/node_var="node[node_id]"
 			if(!(node_var in vars))
-				testing("[node_var] not in vars.")
+				//testing("[node_var] not in vars.")
 				return
 			if(!vars[node_var])
 				vars[node_var] = found

code/game/asteroid.dm
@@ -69,7 +69,7 @@ proc/make_mining_asteroid_secret()
 	while(1)
 		sanity++
 		if(sanity > 100)
-			testing("Tried to place complex too many times.  Aborting.")
+			//testing("Tried to place complex too many times.  Aborting.")
 			return 0
 
 		T=pick(turfs)

code/game/dna/genes/disabilities.dm
@@ -37,8 +37,8 @@
 		M.sdisabilities|=sdisability
 	if(activation_message)
 		to_chat(M, "<span class='warning'>[activation_message]</span>")
-	else
-		testing("[name] has no activation message.")
+//	else
+		//testing("[name] has no activation message.")
 
 /datum/dna/gene/disability/deactivate(var/mob/M, var/connected, var/flags)
 	if(flags & GENE_NATURAL)
@@ -54,8 +54,8 @@
 		M.sdisabilities &= ~sdisability
 	if(deactivation_message)
 		to_chat(M, "<span class='warning'>[deactivation_message]</span>")
-	else
-		testing("[name] has no deactivation message.")
+//	else
+//		testing("[name] has no deactivation message.")
 	return ..()
 
 /datum/dna/gene/disability/hallucinate

code/game/gamemodes/setupgame.dm
@@ -214,7 +214,7 @@ var/LACTOSEBLOCK = 0
 					warning("DNA2: Gene [gene.name] trying to add to already assigned gene block list (used by [english_list(assigned_gene_blocks[block])])")
 				assigned_gene_blocks[block] = gene
 
-	testing("DNA2: [numsToAssign.len] blocks are unused: [english_list(numsToAssign)]")
+	//testing("DNA2: [numsToAssign.len] blocks are unused: [english_list(numsToAssign)]")
 
 // Run AFTER genetics setup and AFTER species setup.
 /proc/setup_species()

code/game/objects/items/mountable_frames/station_map.dm
@@ -103,7 +103,7 @@
 
 /datum/construction/reversible/station_map/spawn_result(mob/user as mob)
 	if(result)
-		testing("[user] finished a [result]!")
+		//testing("[user] finished a [result]!")
 
 		var/obj/machinery/station_map/S = new result(get_turf(holder))
 		S.dir = holder.dir

code/modules/admin/admin_ranks.dm
@@ -74,7 +74,7 @@ var/list/admin_ranks = list()								//list of all ranks with associated rights
 	var/msg = "Permission Sets Built:\n"
 	for(var/rank in admin_ranks)
 		msg += "\t[rank] - [admin_ranks[rank]]\n"
-	testing(msg)
+	//testing(msg)
 	#endif
 
 
@@ -174,7 +174,7 @@ var/list/admin_ranks = list()								//list of all ranks with associated rights
 		if(D)
 			rank = D.rank
 		msg += "\t[ckey] - [rank]\n"
-	testing(msg)
+	//testing(msg)
 	#endif
 
 

commit 1f07092ad863f0c90668503b71c8c8c5e1b9d761

Fixed 2 warnings caused by #18549

code/ATMOSPHERICS/components/binary_devices/dp_vent_pump.dm
@@ -157,51 +157,39 @@
 	if(!signal.data["tag"] || (signal.data["tag"] != id_tag) || (signal.data["sigtype"]!="command"))
 		return 0
 
-	var/handled=0
 	if("power" in signal.data)
 		on = text2num(signal.data["power"])
-		handled=1
 
 	if("power_toggle" in signal.data)
 		on = !on
-		handled=1
 
 	if("direction" in signal.data)
 		pump_direction = text2num(signal.data["direction"])
-		handled=1
 
 	if("checks" in signal.data)
 		pressure_checks = text2num(signal.data["checks"])
-		handled=1
 
 	if("purge" in signal.data)
 		pressure_checks &= ~1
 		pump_direction = 0
-		handled=1
 
 	if("stabilize" in signal.data)
 		pressure_checks |= 1
 		pump_direction = 1
-		handled=1
 
 	if("set_input_pressure" in signal.data)
 		input_pressure_min = Clamp(text2num(signal.data["set_input_pressure"]), 0, ONE_ATMOSPHERE * 50)
-		handled = 1
 
 	if("set_output_pressure" in signal.data)
 		output_pressure_max = Clamp(text2num(signal.data["set_output_pressure"]), 0, ONE_ATMOSPHERE * 50)
-		handled = 1
 
 	if("set_external_pressure" in signal.data)
 		external_pressure_bound = Clamp(text2num(signal.data["set_external_pressure"]), 0, ONE_ATMOSPHERE * 50)
-		handled = 1
 
 	if("status" in signal.data)
 		spawn(2)
 			broadcast_status()
 		return //do not update_icon
-//	if(!handled)
-		//testing("\[[world.timeofday]\]: dp_vent_pump/receive_signal: unknown command \n[signal.debug_print()]")
 	spawn(2)
 		broadcast_status()
 	update_icon()

code/ATMOSPHERICS/components/unary/vent_pump.dm
@@ -223,52 +223,40 @@
 	if(!signal.data["tag"] || (signal.data["tag"] != id_tag) || (signal.data["sigtype"]!="command") || (signal.data["type"] && signal.data["type"] != "vent"))
 		return 0
 
-	var/handled=0
 	if("purge" in signal.data)
 		pressure_checks &= ~1
 		pump_direction = 0
-		handled=1
 
 	if("stabilize" in signal.data)
 		pressure_checks |= 1
 		pump_direction = 1
-		handled = 1
 
 	if("power" in signal.data)
 		on = text2num(signal.data["power"])
-		handled = 1
 
 	if("power_toggle" in signal.data)
 		on = !on
-		handled = 1
 
 	if("checks" in signal.data)
 		pressure_checks = text2num(signal.data["checks"])
-		handled = 1
 
 	if("checks_toggle" in signal.data)
 		pressure_checks = (pressure_checks?0:3)
-		handled = 1
 
 	if("direction" in signal.data)
 		pump_direction = text2num(signal.data["direction"])
-		handled = 1
 
 	if("set_internal_pressure" in signal.data)
 		internal_pressure_bound = Clamp(text2num(signal.data["set_internal_pressure"]), 0, ONE_ATMOSPHERE * 50)
-		handled =1
 
 	if("set_external_pressure" in signal.data)
 		external_pressure_bound = Clamp(text2num(signal.data["set_external_pressure"]), 0, ONE_ATMOSPHERE * 50)
-		handled = 1
 
 	if("adjust_internal_pressure" in signal.data)
 		internal_pressure_bound = Clamp(internal_pressure_bound + text2num(signal.data["adjust_internal_pressure"]), 0, ONE_ATMOSPHERE * 50)
-		handled = 1
 
 	if("adjust_external_pressure" in signal.data)
 		external_pressure_bound = Clamp(external_pressure_bound + text2num(signal.data["adjust_external_pressure"]), 0, ONE_ATMOSPHERE * 50)
-		handled = 1
 
 	if("init" in signal.data)
 		name = signal.data["init"]
@@ -279,8 +267,6 @@
 			broadcast_status()
 		return //do not update_icon
 
-//	if(!handled)
-		//testing("\[[world.timeofday]\]: vent_pump/receive_signal: unknown command \n[signal.debug_print()]")
 	spawn(2)
 		broadcast_status()
 	update_icon()