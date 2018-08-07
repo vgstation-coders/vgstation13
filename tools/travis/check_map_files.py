fd2c835ed93e259499f00786e4efd50b32f8a24a
Shifty/Anthomansland <astanino@gmail.com>
2018-08-08 00:03:55 +0200

    Travis will yell at 'pixel_w'

--- tools/travis/check_map_files.py
+++ tools/travis/check_map_files.py
@@ -24,6 +24,7 @@ BAD_STRINGS = {
     "step_x": re.compile(r"\bstep_x\b"),
     "step_y": re.compile(r"\bstep_y\b"),
     "layer": re.compile(r"\blayer\b")
+    "pixel_w" : re.compile(r"\bpixel_w\b") # This causes the map to require 511/512 to play on.
 }
 
 def main():
