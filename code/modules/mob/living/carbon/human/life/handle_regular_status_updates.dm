vgstation-coders/vgstation13

commit 5875fe60c19f5581a255b1af6a41df6dd84360eb

disability fixes (#21028)

code/modules/mob/living/carbon/human/life/handle_regular_status_updates.dm
@@ -91,17 +91,15 @@
 		else if(!has_eyes())           //Eyes cut out? Permablind.
 			eye_blind =  1
 			blinded =    1
-			eye_blurry = 1
 		else if(sdisabilities & BLIND) //Disabled-blind, doesn't get better on its own
 			blinded =    1
 		else if(eye_blind)		       //Blindness, heals slowly over time
 			eye_blind =  max(eye_blind - 1, 0)
 			blinded =    1
-		else if(istype(glasses, /obj/item/clothing/glasses/sunglasses/blindfold)) //Resting your eyes with a blindfold heals blurry eyes faster
+		if(istype(glasses, /obj/item/clothing/glasses/sunglasses/blindfold)) //Resting your eyes with a blindfold heals blurry eyes faster
 			eye_blurry = max(eye_blurry - 3, 0)
 			blinded =    1
-		else if(eye_blurry)	           //Blurry eyes heal slowly
-			eye_blurry = max(eye_blurry - 1, 0)
+		eye_blurry = max(eye_blurry - 1, 0)
 
 		//Ears
 		if(sdisabilities & DEAF) //Disabled-deaf, doesn't get better on its own

code/game/objects/items/devices/megaphone.dm
@@ -42,7 +42,7 @@
 		if(user.client.prefs.muted & MUTE_IC)
 			to_chat(src, "<span class='warning'>You cannot speak in IC (muted).</span>")
 			return 0
-	if(issilent(user))
+	if(issilent(user) || user.is_mute())
 		to_chat(user, "<span class='warning'>You find yourself unable to speak at all.</span>")
 		return 0
 	if(spamcheck)

code/game/objects/items/toys.dm
@@ -721,7 +721,7 @@
 	if(!ishigherbeing(user))
 		to_chat(user, "<span class = 'warning'>You don't know how to use this!</span>")
 		return
-	if(issilent(user))
+	if(issilent(user) || user.is_mute())
 		to_chat(user, "<span class = 'warning'>You find yourself unable to speak at all.</span>")
 		return
 	if(spamcheck)

code/game/objects/items/weapons/tools.dm
@@ -480,7 +480,8 @@
 /obj/item/weapon/weldingtool/proc/eyecheck(mob/user as mob)
 	if(!iscarbon(user))
 		return 1
-	var/safety = user:eyecheck()
+	var/mob/living/carbon/C = user //eyecheck is living-level
+	var/safety = C.eyecheck()
 	if(istype(user, /mob/living/carbon/human))
 		var/mob/living/carbon/human/H = user
 		var/datum/organ/internal/eyes/E = H.internal_organs_by_name["eyes"]
@@ -489,7 +490,7 @@
 		if(E.welding_proof)
 			user.simple_message("<span class='notice'>Your eyelenses darken to accommodate for the welder's glow.</span>")
 			return
-		if(safety < 2 && eye_damaging)
+		if(safety && eye_damaging && !(user.sdisabilities & BLIND))
 			switch(safety)
 				if(1)
 					user.simple_message("<span class='warning'>Your eyes sting a little.</span>",\

commit c99db84a76d32c3ef809ec93e2eb5fcf78ca13e6

Stops eye_blurry goind down while eye_blind is >0
Readds the previous functionality whereby eye_blurry will not go down
until eye_blind has healed. This was previously broken by #21028
Retains the change which makes characters with removed eyes have nonblurry
(blind) vision as per disabled blind characters.
Reverts blindfolds to healing eye_blurry under the same conditions and
at the same rate as previously also.

code/modules/mob/living/carbon/human/life/handle_regular_status_updates.dm
@@ -91,15 +91,18 @@
 		else if(!has_eyes())           //Eyes cut out? Permablind.
 			eye_blind =  1
 			blinded =    1
+			eye_blurry = 0
 		else if(sdisabilities & BLIND) //Disabled-blind, doesn't get better on its own
 			blinded =    1
+			eye_blurry = 0
 		else if(eye_blind)		       //Blindness, heals slowly over time
 			eye_blind =  max(eye_blind - 1, 0)
 			blinded =    1
-		if(istype(glasses, /obj/item/clothing/glasses/sunglasses/blindfold)) //Resting your eyes with a blindfold heals blurry eyes faster
+		else if(istype(glasses, /obj/item/clothing/glasses/sunglasses/blindfold)) //Resting your eyes with a blindfold heals blurry eyes faster
 			eye_blurry = max(eye_blurry - 3, 0)
 			blinded =    1
-		eye_blurry = max(eye_blurry - 1, 0)
+		else if(eye_blurry)
+			eye_blurry = max(eye_blurry - 1, 0)
 
 		//Ears
 		if(sdisabilities & DEAF) //Disabled-deaf, doesn't get better on its own