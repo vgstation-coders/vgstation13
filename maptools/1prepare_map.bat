d5504ab51d8bed415e233c98a5ced0e48bbb70eb
brndd <burneddi@gmail.com>
2019-11-04 01:21:56 +0200

    shell script too

--- maptools/1prepare_map.bat
+++ maptools/1prepare_map.bat
@@ -4,7 +4,7 @@ set MAPFILE_MS=metaclub.dmm
 set MAPFILE_PCK=packedstation.dmm
 set MAPFILE_ROID=RoidStation.dmm
 set MAPFILE_SNAXI=snaxi.dmm
-set MAPFILE_WHEELSTATION=wheelstation.dmm
+set MAPFILE_WHEEL=wheelstation.dmm
 
 cd ../maps
 copy %MAPFILE_TG% %MAPFILE_TG%.backup
@@ -13,6 +13,6 @@ copy %MAPFILE_MS% %MAPFILE_MS%.backup
 copy %MAPFILE_PCK% %MAPFILE_PCK%.backup
 copy %MAPFILE_ROID% %MAPFILE_ROID%.backup
 copy %MAPFILE_SNAXI% %MAPFILE_SNAXI%.backup
-copy %MAPFILE_WHEELSTATION% %MAPFILE_WHEELSTATION%.backup
+copy %MAPFILE_WHEEL% %MAPFILE_WHEEL%.backup
 
 pause
