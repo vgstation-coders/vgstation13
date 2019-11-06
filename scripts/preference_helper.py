66a00c82bfeaeb71a1793ff056042f70ff76ae8e
Shifty/Anthomansland <astanino@gmail.com>
2019-11-06 11:48:24 +0100

    Derp

--- scripts/preference_helper.py
+++ scripts/preference_helper.py
@@ -4,7 +4,7 @@
 
 import re
 
-file_path = "./code/modules/client/preferences_savefile.dm"
+file_path = "../code/modules/client/preferences_savefile.dm"
 
 # 1. Reading the variable name
 var_name = str(input("Input the name of the new preference."))
@@ -12,14 +12,29 @@ var_name = str(input("Input the name of the new preference."))
 # 2. Editing the sql queries 
 with open(file_path) as f:
     for line in f.readlines():
-        if "INSERT into client" in line
-            line.replace("[^\?]\)", " ,"+var_name+")" % (f)) # No "?" and a closing parenthesis ; replace ")" by ", var_name)"
-            line.replace("\?\)", "?, ?)" % (f)) # replace "?)" by "?, ?)"
-            line = reader.readline()
-            line.replace("[^\?]\)", " ,"+var_name+")" % (f)) # No "?" and a closing parenthesis ; replace ")" by ", var_name)"
-            print("Changed the INSERT into client")
-        if "UPDATE client"
-            line.replace("WHERE", var_name+"=?"+" WHERE" % (f)) # replace "WHERE" by "var_name=?, WHERE"
-            line = reader.readline()
-            line.replace(", ckey)", ","+var_name+", ckey)" % (f)) # replace ",ckey)" by "var_name, ckey)"
-            print("Changed the UPDATE client")
+        if "INSERT into client" in line:
+            print(line)
+            new_string = ", "+var_name+")"
+            line = re.sub("[^\?]\)", new_string, line) # No "?" and a closing parenthesis ; replace ")" by ", var_name)"
+            line = re.sub("\?\)", "?,?)", line) # replace "?)" by "?, ?)"
+            print(line)
+            print("Change the first line into the second one.")
+            line = f.readline()
+            print(line)
+            line = re.sub("[^\?]\)", new_string, line) # No "?" and a closing parenthesis ; replace ")" by ", var_name)"
+            print(line)
+            print("Change the first line into the second one.")
+        if "UPDATE client SET ooc" in line:
+            new_string = ", "+var_name+"=? WHERE"
+            print(line)
+            line = re.sub("WHERE", new_string, line) # replace "WHERE" by "var_name=?, WHERE"
+            print(line)
+            print("Change the first line into the second one.")
+            line = f.readline()
+            new_string = ","+var_name+", ckey)"
+            print(line)
+            line = re.sub(", ckey\)", new_string, line) # replace ",ckey)" by "var_name, ckey)"
+            print(line)
+            print("Change the first line into the second one.")
+
+f.close()
