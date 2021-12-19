c4be253dec09db0bf7b95744f6b140f1a90fc5d3
jellyveggie2 <78439377+jellyveggie2@users.noreply.github.com>
2021-12-19 09:45:59 +0100

    Template width, offset, and import error messages

--- code/modules/html_interface/paintTool/canvas.js
+++ code/modules/html_interface/paintTool/canvas.js
@@ -41,9 +41,11 @@ function toggleHelp(helpButton, helpPanel) {
 //--------------------------------
 // Template control
 //--------------------------------
-var currentTemplate = {};
+var currentTemplate = {ox:0, oy:0};
 /* A test template for 14x14 sized paintings
 {
+	"w":14,
+	"h":14,
 	"rgn":[
 		{"clr":"#ffffff","txt":"Mask"},
 		{"clr":"#ffdddd","txt":"Mask border"},
@@ -74,6 +76,7 @@ var currentTemplate = {};
 }
 */
 
+// Moves template color regions from/to "pending" and "done" lists
 function templateMover(id, isTargetToDone) {
 	var item = document.getElementById("tp-" + id);
 
@@ -109,15 +112,11 @@ function templateMover(id, isTargetToDone) {
 	if (!inserted) document.getElementById(target).insertAdjacentElement("beforeend", item);
 }
 
+// Shorthand for templateMover()
+function templatePending(id) { templateMover(id, false); }
+function templateDone(id) { templateMover(id, true); }
 
-function templatePending(id) {
-	templateMover(id, false);
-}
-
-function templateDone(id) {
-	templateMover(id, true);
-}
-
+// Builds color region items for the pending/done region list in the templates tab
 var colorRegex = /^#[0-9A-Fa-f]{6}$/;
 function buildTemplateItem(id, colorHint, textHint) {
 	// Create a new color region element
@@ -127,7 +126,7 @@ function buildTemplateItem(id, colorHint, textHint) {
 
 	// Color hint. Invalid colors get a red cross over a white background instead
 	var missingColorIcon = "";
-	if (!colorRegex.test(colorHint)) {
+	if (!colorHint || !colorRegex.test(colorHint)) {
 		missingColorIcon = '<span style="color: #ff0000; font-weight: bold">X</span>';
 		colorHint = "#ffffff";
 	}
@@ -164,8 +163,119 @@ function buildTemplateItem(id, colorHint, textHint) {
 
 }
 
+// Paints the specified color region with the current color and opacity
+function templatePaint(id) {
+	for (i in currentTemplate.bmp) {
+		if (currentTemplate.bmp[i] == id) {
+			var x = i % currentTemplate.w;
+			var y = (i - x)/currentTemplate.w;
+			x += currentTemplate.ox;
+			y -= currentTemplate.oy;
+			if (x >= 0 && x < width && y >= 0 && y < height)
+				pixelDraw(x, y, getPaintColor(), getOpacity());
+		}
+	}
+	display_bitmap();
+	document.getElementById("bitmap").value = bitmap;
+}
+
+// Manipulate template offset along the X axis
+function setTemplateOffsetX() {
+	var input = document.getElementById("template_offset_X");
+	currentTemplate.ox = parseInt(input.value);
+	currentTemplate.ox = isNaN(currentTemplate.ox) ? 0 : currentTemplate.ox;
+	input.value = currentTemplate.ox;
+}
+
+function changeTemplateOffsetX(diff) {
+	var input = document.getElementById("template_offset_X");
+	currentTemplate.ox += diff ? diff : 0;
+	input.value = currentTemplate.ox;
+}
+
+// Manipulate template offset along the Y axis
+function setTemplateOffsetY() {
+	var input = document.getElementById("template_offset_Y");
+	currentTemplate.oy = parseInt(input.value);
+	currentTemplate.oy = isNaN(currentTemplate.oy) ? 0 : currentTemplate.oy;
+	input.value = currentTemplate.oy;
+}
+
+function changeTemplateOffsetY(diff) {
+	var input = document.getElementById("template_offset_Y");
+	currentTemplate.oy += diff ? diff : 0;
+	input.value = currentTemplate.oy;
+}
+
+//--------------------------------
+// Template import
+//--------------------------------
+
+
+// Returns the parsed template if succesful, throws an error message otherwise
+function parseTemplate(template) {
+	// Check the template contains anything resembling a JSON
+	var tmpTemplate = template.slice(template.indexOf('{'), template.lastIndexOf('}')+1);
+	if (!tmpTemplate) throw "Input is gibberish and not a template";
+	
+	// Attempt to parse the template
+	try {
+		tmpTemplate = JSON.parse(tmpTemplate);
+	} catch(e) {
+		throw "Template could not be parsed:<span style=\"font-style:italic\">" + e.message.slice(e.message.indexOf(':')+1)+"</span>";
+	}
+	
+	// Check template contains a valid "rgn"
+	if (!tmpTemplate.rgn) throw "Template is missing <code>\"rgn\"</code> parameter.";
+	if (!Array.isArray(tmpTemplate.rgn) || !tmpTemplate.rgn.length) throw "<code>\"rgn\"</code> parameter is empty or not an array.";
+	
+	// Check template contains a valid "bmp"
+	if (!tmpTemplate.bmp) throw "Template is missing <code>\"bmp\"</code> parameter.";
+	if (!Array.isArray(tmpTemplate.bmp) || !tmpTemplate.bmp.length) throw "<code>\"bmp\"</code> parameter is empty or not an array.";
+	return tmpTemplate;
+}
+
+// Detects any potential issues with the template and returns the appropiate message, returns an empty string if no issues are found
+function lintTemplate(template) {
+	if (!template.w && (template.bmp.length % width) != 0)
+		return "Note: Template bitmap (" + template.bmp.length + "px) does not match canvas width (" + width + "px), deformation is likely. ";
+	if (!template.w && template.bmp.length != (height * width))
+		return "Note: Template bitmap (" + template.bmp.length + "px) does not match canvas size (" + width + "x" + height + " = "+ width * height + "px)";
+	if (template.w && (template.bmp.length % template.w) != 0)
+		return "Note: Template bitmap (" + template.bmp.length + "px) does not match template width (" + template.w + "px), deformation is likely";
+	if (template.w && template.h && template.bmp.length != (template.w * template.h))
+		return "Note: Template bitmap (" + template.bmp.length + "px) does not match template size (" + template.w + "x" + template.h + " = "+ template.w * template.h + "px), height may be incorrect";
+	if (template.w && template.w > width || template.h && template.h > height)
+		return "Note: Template (" + (template.w ? template.w : "??") + "x" + (template.h ? template.h : "??") + ") will not fit canvas (" + width + "x" + height + ")";
+}
+
+// Loads a template
 function loadTemplate(template) {
-	//Empty both Pending and Done lists
+	// Hide error messages
+	document.getElementById("template_import_errors").style.display = "none";
+	document.getElementById("template_import_warnings").style.display = "none";
+	
+	// Parse template
+	if (!template) return;
+	var tmpTemplate;
+	try {
+		tmpTemplate	= parseTemplate(template);
+	} catch (msg) {
+		// Display error message
+		document.getElementById("template_import_errors").style.display = "block";
+		document.getElementById("template_import_errors").innerHTML = msg;
+		return;
+	}
+	currentTemplate = tmpTemplate;
+	
+	// Warn the user of any potential issues
+	var warning = lintTemplate(currentTemplate);
+	if (warning) {
+		document.getElementById("template_import_warnings").style.display = "block";
+		document.getElementById("template_import_warnings").innerHTML = warning;
+	}
+
+	// Empty both Pending and Done lists
 	var itemLister = document.getElementById("doneRegions");
 	while (itemLister.childElementCount > 0) {
 		itemLister.removeChild(itemLister.firstChild);
@@ -176,16 +286,41 @@ function loadTemplate(template) {
 	}
 
 	// Fill Pending list with new color regions
-	currentTemplate = JSON.parse(template);
 	for (i = 0; i < currentTemplate.rgn.length; i++) {
 		itemLister.appendChild(buildTemplateItem(i, currentTemplate.rgn[i].clr, currentTemplate.rgn[i].txt));
 	}
+	
+	// Set up template size display
+	if (currentTemplate.w || currentTemplate.h) {
+		document.getElementById("template_size").style.display = "block";
+		document.getElementById("template_width").innerHTML = currentTemplate.w ? currentTemplate.w : "??";
+		document.getElementById("template_height").innerHTML = currentTemplate.h ? currentTemplate.h : "??";
+	} else {
+		document.getElementById("template_size").style.display = "none";
+	}
+	
+	// Set up default template width to canvas width if missing
+	currentTemplate.w = currentTemplate.w ? currentTemplate.w : width;
+	
+	// Set up initial template offset, if missing
+	currentTemplate.ox = currentTemplate.ox ? currentTemplate.ox : 0;
+	currentTemplate.oy = currentTemplate.oy ? currentTemplate.oy : 0;
+	
+	changeTemplateOffsetX();
+	changeTemplateOffsetY();
+	
+	// Open up the template tab and save the user that extra click
+	panelSelect('templatePanel');
 }
 
+//--------------------------------
+// Template export
+//--------------------------------
+
+// Exports the current bitmap as a template
 function exportTemplate() {
 	var output = document.getElementById("export-text");
-	var template = {rgn: [], bmp: []};
-
+	var template = {w: width, h: height, rgn: [], bmp: []};
 	var colors = [];
 	for (pixel in bitmap) {
 		if (colors.indexOf(bitmap[pixel]) < 0) {
@@ -195,18 +330,6 @@ function exportTemplate() {
 		template.bmp.push(colors.indexOf(bitmap[pixel]));
 	}
 	output.value = JSON.stringify(template);
-}
-
-function templatePaint(id) {
-	for (i in currentTemplate.bmp) {
-		if (currentTemplate.bmp[i] == id) {
-			var x = i % width;
-			var y = (i - x)/width;
-			pixelDraw(x, y, getPaintColor(), getOpacity());
-		}
-	}
-	display_bitmap();
-	document.getElementById("bitmap").value = bitmap;
 }
 
 //--------------------------------
@@ -221,6 +344,9 @@ function initCanvas(paintInitData, canvasInitData) {
 	canvasInitData = JSON.parse(canvasInitData);
 
 	src = canvasInitData.src;
+	document.getElementById("canvas_width").innerHTML = width;
+	document.getElementById("canvas_height").innerHTML = height;
+	
 	document.getElementById("title").value = canvasInitData.title;
 	document.getElementById("author").value = canvasInitData.author;
 	document.getElementById("description").value = canvasInitData.description;
@@ -237,7 +363,7 @@ function initCanvas(paintInitData, canvasInitData) {
 	
 	for (color in palette) {
 		paletteButtonPanel.innerHTML +=
-			'<div class="paletteColor" onclick="setColor(\'' + palette[color] + '\');" style="background-image:' + generateColorPaletteBackgroundStyle(palette[color]) + '; background-image:' + generateColorPaletteBackgroundStyle(palette[color], true) + '"></div>\n';
+			'<div class="paletteColor" onclick="setColor(\'' + palette[color] + '\');" style="background-image:' +  generateColorPaletteBackgroundStyle(palette[color]) + '; background-image:' +  generateColorPaletteBackgroundStyle(palette[color], true) + '"></div>\n';
 	}
 	setColor(palette[0]);
 }
