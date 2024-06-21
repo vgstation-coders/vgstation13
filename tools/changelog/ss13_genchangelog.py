f7ee97d76d178d5d4b6bfd1ef169e846c2f7060e
toomykins <29069174+toomykins@users.noreply.github.com>
2024-06-21 02:20:12 +0200

    Update ss13_genchangelog.py

--- tools/changelog/ss13_genchangelog.py
+++ tools/changelog/ss13_genchangelog.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python3
-'''
-Dependencies:
-    Beautiful Soup 4 (for now)
-    PyYAML
-    Jinja2
+"""
 Usage:
     $ python ss13_genchangelog.py [--dry-run] html/changelog.html html/changelogs/
 
 ss13_genchangelog.py - Generate changelog from YAML.
 
-Copyright (C) 2013-2019 Rob "N3X15" Nelson <nexisentertainment@gmail.com>
+Copyright 2013 Rob "N3X15" Nelson <nexis@7chan.org>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -28,26 +23,47 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-'''
+"""
 
-import sys
-if sys.version_info[0] < 3:
-    raise Exception("Must be using Python 3")
-if sys.version_info.minor < 6:
-    raise Exception("Must be using Python >= 3.6")
+from __future__ import print_function
 
-from time import time
 import argparse
-import collections
-import datetime
 import glob
-import jinja2
-import logging
 import os
+import sys
+from datetime import date, datetime, timedelta
+
 import yaml
 
-# Valid entry types. Update changelog.schema.yml if you fuck with this.
-VALID_PREFIXES = [
+today = date.today()
+
+dateformat = "%d %B %Y"
+
+opt = argparse.ArgumentParser()
+opt.add_argument(
+    "-d",
+    "--dry-run",
+    dest="dryRun",
+    default=False,
+    action="store_true",
+    help="Only parse changelogs and, if needed, the targetFile. (A .dry_changelog.yml will be output for debugging purposes.)",
+)
+opt.add_argument(
+    "-t",
+    "--time-period",
+    dest="timePeriod",
+    default=9,
+    type=int,
+    help="Define how many weeks back the changelog should display",
+)
+opt.add_argument("targetFile", help="The HTML changelog we wish to update.")
+opt.add_argument("ymlDir", help="The directory of YAML changelogs we will use.")
+
+args = opt.parse_args()
+
+all_changelog_entries = {}
+
+validPrefixes = [
     'bugfix',
     'experiment',
     'imageadd',
@@ -62,41 +78,66 @@ VALID_PREFIXES = [
     'wip',
 ]
 
-# So changelog isn't 5000 entries long
-MAX_DATE_ENTRIES = 100
 
-# Date format to use.
-DATEFORMAT = "%Y.%m.%d"
+def dictToTuples(inp):
+    return [(k, v) for k, v in inp.items()]
 
-def rebuild_changelog_from(old_changelog: str, all_changelog_entries: dict):
+
+changelog_cache = os.path.join(args.ymlDir, ".all_changelog.yml")
+
+failed_cache_read = True
+if os.path.isfile(changelog_cache):
+    try:
+        with open(changelog_cache, encoding="utf-8") as f:
+            (_, all_changelog_entries) = yaml.safe_load_all(f)
+            failed_cache_read = False
+
+            # Convert old timestamps to newer format.
+            new_entries = {}
+            for _date in all_changelog_entries.keys():
+                ty = type(_date).__name__
+                # print(ty)
+                if ty in ["str", "unicode"]:
+                    temp_data = all_changelog_entries[_date]
+                    _date = datetime.strptime(_date, dateformat).date()
+                    new_entries[_date] = temp_data
+                else:
+                    new_entries[_date] = all_changelog_entries[_date]
+            all_changelog_entries = new_entries
+    except Exception as e:
+        print("Failed to read cache:")
+        print(e, file=sys.stderr)
+
+if args.dryRun:
+    changelog_cache = os.path.join(args.ymlDir, ".dry_changelog.yml")
+
+if failed_cache_read and os.path.isfile(args.targetFile):
     from bs4 import BeautifulSoup
-    #from bs4.element import NavigableString
-    logging.info('Generating cache...')
-    with open(old_changelog, 'r') as f:
-        soup = BeautifulSoup(f, features='lxml')
-        # Thankfully, old-style changelogs used a fairly standardized layout.
-        for e in soup.find_all('div', {'class': 'commit'}):
+
+    print(" Generating cache...")
+    with open(args.targetFile, "r", encoding="utf-8") as f:
+        soup = BeautifulSoup(f, features="html.parser")
+        for e in soup.find_all("div", {"class": "commit"}):
             entry = {}
-            # I love how redundant Python's STL is.
-            date = datetime.datetime.strptime(e.h2.string.strip(), DATEFORMAT).date()  # key
-            for authorT in e.find_all('h3', {'class': 'author'}):
+            date = datetime.strptime(e.h2.string.strip(), dateformat).date()  # key
+            for authorT in e.find_all("h3", {"class": "author"}):
                 author = authorT.string
                 # Strip suffix
-                if author.endswith('updated:'):
+                if author.endswith("updated:"):
                     author = author[:-8]
                 author = author.strip()
 
                 # Find <ul>
                 ulT = authorT.next_sibling
-                while(ulT.name != 'ul'):
+                while ulT.name != "ul":
                     ulT = ulT.next_sibling
                 changes = []
 
                 for changeT in ulT.children:
-                    if changeT.name != 'li':
+                    if changeT.name != "li":
                         continue
                     val = changeT.decode_contents(formatter="html")
-                    newdat = {changeT['class'][0] + '': val + ''}
+                    newdat = {changeT["class"][0] + "": val + ""}
                     if newdat not in changes:
                         changes += [newdat]
 
@@ -107,158 +148,98 @@ def rebuild_changelog_from(old_changelog: str, all_changelog_entries: dict):
             else:
                 all_changelog_entries[date] = entry
 
-def loadCache(changelog_cachefile):
-    failed_cache_read = True
-    all_changelog_entries = {}
-    try:
-        with open(changelog_cachefile) as f:
-            (_, all_changelog_entries) = yaml.safe_load_all(f)
-            failed_cache_read = False
+del_after = []
+print("Reading changelogs...")
+for fileName in glob.glob(os.path.join(args.ymlDir, "*.yml")):
+    name, ext = os.path.splitext(os.path.basename(fileName))
+    if name.startswith("."):
+        continue
+    if name == "example":
+        continue
+    fileName = os.path.abspath(fileName)
+    print(" Reading {}...".format(fileName))
+    cl = {}
+    with open(fileName, "r", encoding="utf-8") as f:
+        cl = yaml.safe_load(f)
+        f.close()
+    if today not in all_changelog_entries:
+        all_changelog_entries[today] = {}
+    author_entries = all_changelog_entries[today].get(cl["author"], [])
+    if len(cl["changes"]):
+        new = 0
+        for change in cl["changes"]:
+            if change not in author_entries:
+                (change_type, _) = dictToTuples(change)[0]
+                if change_type not in validPrefixes:
+                    print("  {0}: Invalid prefix {1}".format(fileName, change_type), file=sys.stderr)
+                author_entries += [change]
+                new += 1
+        all_changelog_entries[today][cl["author"]] = author_entries
+        if new > 0:
+            print("  Added {0} new changelog entries.".format(new))
 
-            # Convert old timestamps to newer format.
-            new_entries = {}
-            for _date in all_changelog_entries.keys():
-                ty = type(_date).__name__
-                # print(ty)
-                if ty in ['str', 'unicode']:
-                    temp_data = all_changelog_entries[_date]
-                    _date = datetime.datetime.strptime(_date, DATEFORMAT).date()
-                    new_entries[_date] = temp_data
-                else:
-                    new_entries[_date] = all_changelog_entries[_date]
-            all_changelog_entries = new_entries
-    except Exception as e:
-        logging.error("Failed to read cache:")
-        logging.exception(e)
-    return (failed_cache_read, all_changelog_entries)
+    if cl.get("delete-after", False):
+        if os.path.isfile(fileName):
+            if args.dryRun:
+                print("  Would delete {0} (delete-after set)...".format(fileName))
+            else:
+                del_after += [fileName]
 
-def main():
-    opt = argparse.ArgumentParser()
-    opt.add_argument('-d', '--dry-run', dest='dryRun', default=False, action='store_true', help='Only parse changelogs and, if needed, the targetFile. (A .dry_changelog.yml will be output for debugging purposes.)')
-    opt.add_argument('targetFile', help='The HTML changelog we wish to update.')
-    opt.add_argument('ymlDir', help='The directory of YAML changelogs we will use.')
+    if args.dryRun:
+        continue
 
-    args = opt.parse_args()
+    cl["changes"] = []
+    with open(fileName, "w", encoding="utf-8") as f:
+        yaml.dump(cl, f, default_flow_style=False)
 
-    today = datetime.date.today()
+targetDir = os.path.dirname(args.targetFile)
 
-    all_changelog_entries = {}
+with open(
+    args.targetFile.replace(".htm", ".dry.htm") if args.dryRun else args.targetFile, "w", encoding="utf-8"
+) as changelog:
+    with open(os.path.join(targetDir, "templates", "header.html"), "r", encoding="utf-8") as h:
+        for line in h:
+            changelog.write(line)
 
-    changelog_cache = os.path.join(args.ymlDir, '.all_changelog.yml')
-
-    failed_cache_read = True
-    if not args.dryRun:
-        if os.path.isfile(changelog_cache):
-            (failed_cache_read, all_changelog_entries) = loadCache(changelog_cache)
-    else:
-        changelog_cache = os.path.join(args.ymlDir, '.dry_changelog.yml')
-
-    if failed_cache_read and os.path.isfile(args.targetFile):
-        rebuild_changelog_from(args.targetFile, all_changelog_entries)
-
-    del_after = []
-    logging.info('Reading changelogs...')
-    for fileName in glob.glob(os.path.join(args.ymlDir, "*.yml")):
-        name, _ = os.path.splitext(os.path.basename(fileName))
-        if name.startswith('.'):
+    weekstoshow = timedelta(weeks=args.timePeriod)
+    for _date in reversed(sorted(all_changelog_entries.keys())):
+        if not (today - _date < weekstoshow):
             continue
-        if name == 'example':
-            continue
-        fileName = os.path.abspath(fileName)
-        logging.info('  Reading {}...'.format(fileName))
-        cl = {}
-        with open(fileName, 'r') as f:
-            cl = yaml.safe_load(f)
-            f.close()
-        if today not in all_changelog_entries:
-            all_changelog_entries[today] = {}
-        author_entries = all_changelog_entries[today].get(cl['author'], [])
-        if len(cl['changes']):
-            new = 0
-            for change in cl['changes']:
-                if change not in author_entries:
-                    (change_type, _) = next(iter(change.items()))
-                    if change_type not in VALID_PREFIXES:
-                        logging.critical('  {0}: Invalid prefix {1}'.format(fileName, change_type))
-                        return
-                    author_entries += [change]
-                    new += 1
-            all_changelog_entries[today][cl['author']] = author_entries
-            if new > 0:
-                logging.info('    Added {0} new changelog entries.'.format(new))
-
-        if cl.get('delete-after', False):
-            if os.path.isfile(fileName):
-                if args.dryRun:
-                    logging.warning('  Would delete {0} (delete-after set)...'.format(fileName))
-                else:
-                    del_after += [fileName]
-
-        if args.dryRun:
-            continue
-
-        cl['changes'] = []
-        with open(fileName, 'w') as f:
-            yaml.dump(cl, f, default_flow_style=False)
-
-    targetDir = os.path.dirname(args.targetFile)
-
-    jenv = jinja2.Environment(
-        extensions=['jinja2.ext.do'],  # Occasionally useful.
-        loader=jinja2.FileSystemLoader('.'),
-        autoescape=jinja2.select_autoescape(
-            enabled_extensions=('htm', 'html'),
-        ))
-    tmpl = jenv.get_template(os.path.join(targetDir, 'templates', 'changelog.tmpl.htm'))
-
-
-    remove_dates = []
-    days_written = 0
-    entries = collections.OrderedDict()
-    for _date in sorted(all_changelog_entries.keys(), reverse=True):
-        #entry_htm = '\n'
-        #entry_htm += '\t\t\t<h2 class="date">{date}</h2>\n'.format(date=_date.strftime(dateformat))
+        entry_htm = "\n"
+        entry_htm += '\t\t\t<h2 class="date">{date}</h2>\n'.format(date=_date.strftime(dateformat))
         write_entry = False
-        date_entries = collections.OrderedDict()
         for author in sorted(all_changelog_entries[_date].keys()):
             if len(all_changelog_entries[_date]) == 0:
                 continue
-            #author_htm = '\t\t\t<h3 class="author">{author} updated:</h3>\n'.format(author=author)
-            #author_htm += '\t\t\t<ul class="changes bgimages16">\n'
+            author_htm = '\t\t\t<h3 class="author">{author} updated:</h3>\n'.format(author=author)
+            author_htm += '\t\t\t<ul class="changes bgimages16">\n'
             changes_added = []
-            for (css_class, change) in (next(iter(e.items())) for e in all_changelog_entries[_date][author]):
+            for (css_class, change) in (dictToTuples(e)[0] for e in all_changelog_entries[_date][author]):
                 if change in changes_added:
                     continue
                 write_entry = True
                 changes_added += [change]
-                #author_htm += '\t\t\t\t<li class="{css_class}">{change}</li>\n'.format(css_class=css_class, change=change.strip())
-                if author not in date_entries:
-                    date_entries[author] = []
-                date_entries[author] += [(css_class, change)]
-            #author_htm += '\t\t\t</ul>\n'
-        if write_entry and days_written <= MAX_DATE_ENTRIES:
-            entries[_date] = date_entries
-            days_written += 1
-        else:
-            remove_dates.append(_date)
+                author_htm += '\t\t\t\t<li class="{css_class}">{change}</li>\n'.format(
+                    css_class=css_class, change=change.strip()
+                )
+            author_htm += "\t\t\t</ul>\n"
+            if len(changes_added) > 0:
+                entry_htm += author_htm
+        if write_entry:
+            changelog.write(entry_htm)
 
-    with open(args.targetFile.replace('.htm', '.dry.htm') if args.dryRun else args.targetFile, 'w') as changelog:
-        changelog.write(tmpl.render(ENTRIES=entries, DATEFORMAT=DATEFORMAT))
+    with open(os.path.join(targetDir, "templates", "footer.html"), "r", encoding="utf-8") as h:
+        for line in h:
+            changelog.write(line)
 
-    for _date in remove_dates:
-        del all_changelog_entries[_date]
-        logging.info('Removing {} (old/invalid)'.format(_date))
 
-    with open(changelog_cache, 'w') as f:
-        cache_head = 'DO NOT EDIT THIS FILE BY HAND!  AUTOMATICALLY GENERATED BY ss13_genchangelog.py.'
-        yaml.dump_all([cache_head, all_changelog_entries], f, default_flow_style=False)
+with open(changelog_cache, "w") as f:
+    cache_head = "DO NOT EDIT THIS FILE BY HAND!  AUTOMATICALLY GENERATED BY ss13_genchangelog.py."
+    yaml.dump_all([cache_head, all_changelog_entries], f, default_flow_style=False)
 
-    if len(del_after):
-        print('Cleaning up...')
-        for fileName in del_after:
-            if os.path.isfile(fileName):
-                print(' Deleting {0} (delete-after set)...'.format(fileName))
-                os.remove(fileName)
-
-if __name__ == '__main__':
-    main()
+if len(del_after):
+    print("Cleaning up...")
+    for fileName in del_after:
+        if os.path.isfile(fileName):
+            print(" Deleting {0} (delete-after set)...".format(fileName))
+            os.remove(fileName)
