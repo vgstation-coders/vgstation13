0519cc45afd72e34bf5a42fbc971e431b3f4e39e
Inorien <Inorien@live.com>
2025-12-22 23:04:19 +0900

    fix a fuck ton of tgui errors and warnings

--- tgui/packages/tgui/interfaces/RoboticsControlConsole.tsx
+++ tgui/packages/tgui/interfaces/RoboticsControlConsole.tsx
@@ -28,7 +28,7 @@ type Cyborg = {
 
 export const RoboticsControlConsole = (props) => {
   const { act, data } = useBackend<Data>();
-  const [tab, setTab] = useState();
+  const [tab, setTab] = useState<number>(2);
   const {
     can_hack,
     sequence_activated,
@@ -116,13 +116,12 @@ const Cyborgs = (props) => {
         )}>
         <Flex>
           <Flex.Item>
-            <Box
-              as="img"
+            <img
               src={`data:image/jpeg;base64,${cyborg.borgimage}`}
               height="64px"
               width="64px"
               style={{
-                'image-rendering': 'pixelated',
+                imageRendering: 'pixelated',
               }}
             />
           </Flex.Item>
@@ -177,8 +176,7 @@ const SelfDestruct = (props) => {
 
   return (
     <NoticeBox
-      info={sequence_activated ? false : true}
-      danger={sequence_activated ? true : false}
+      danger={!!sequence_activated}
       textAlign="center">
 
       Cyborg Emergency Killswitch<br /><br />
